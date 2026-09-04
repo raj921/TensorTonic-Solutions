02b16cfaaf003f030ec7a52a19f15f49eb53cb3b
raj kumar <raj315920@gmail.com>
2026-09-04 22:33:25 +0530

    Solve matrix-transpose

---
+++ matrix-transpose/matrix-transpose.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def matrix_transpose(A: list) -> np.ndarray:
+    """
+    Returns the transposed matrix as a NumPy array.
+    """
+    # Write code here
+    b = np.array(A)
+    c = b.T
+    return c
