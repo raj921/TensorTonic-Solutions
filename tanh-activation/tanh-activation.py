16721e8a2bf9a1f16cc7486b859d9f9157b00335
raj kumar <raj315920@gmail.com>
2026-09-12 15:28:28 +0530

    Solve tanh-activation

---
+++ tanh-activation/tanh-activation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def tanh(x: list) -> np.ndarray:
+    """
+    Returns a NumPy array with the same shape as x.
+    """
+    # Write code here
+    np.asarray(x, dtype=float)
+    y = np.tanh(x)
+    return y 
